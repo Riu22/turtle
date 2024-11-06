0804c34483758e11417bc126681f366936310623
Riu22 <riutordjaume@gmail.com>
2024-11-06 20:21:42 +0100

    Initial commit

---
+++ src/exercici1.java
@@ -0,0 +1,42 @@
+import java.util.Scanner;
+
+public class exercici1 {
+    public static void main(String[] args) {
+        Scanner sc =new Scanner(System.in);
+        System.out.println("dime el numero de lineas ");
+        int rows = sc.nextInt();
+        System.out.println("dime el numeros de columnas");
+        int columns = sc.nextInt();
+        int sideLength = 50;
+        int spacing = 10;
+        Turtle t = new Turtle(500, 500);
+
+        for (int i = 0; i < rows; i++) {
+            for (int j = 0; j < columns; j++) {
+                cuadradito(t, sideLength);
+                t.setPenDown(false);
+                t.forward(sideLength + spacing);
+                t.setPenDown(true);
+            }
+            moverme(t,sideLength, spacing, columns);
+        }
+
+        t.show();
+    }
+
+    public static void cuadradito(Turtle t, int lados) {
+        for (int i = 0; i < 4; i++) {
+            t.forward(lados);
+            t.turnRight(90);
+        }
+    }
+    public static void moverme(Turtle t, int sideLength, int spacing, int columns){
+        t.setPenDown(false);
+        t.turnRight(90);
+        t.forward(sideLength + spacing);
+        t.turnRight(90);
+        t.forward(columns * (sideLength + spacing));
+        t.turnRight(180);
+        t.setPenDown(true);
+    }
+}
