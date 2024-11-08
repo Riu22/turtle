91484e71d4a563363b9c20098524db02d4737d62
Riu22 <riutordjaume@gmail.com>
2024-11-08 15:30:05 +0100

    hecho el 2

--- src/Turtle1.java
+++ src/Turtle1.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-public class exercici1 {
+public class Turtle1 {
     public static void main(String[] args) {
         Scanner sc =new Scanner(System.in);
         System.out.println("dime el numero de lineas ");
---
+++ src/turtle2.java
@@ -0,0 +1,36 @@
+import java.util.Scanner;
+
+public class turtle2 {
+    public static void main(String arg[]){
+        Turtle t = new Turtle(500, 500);
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Dame cuantos escalones tengo que hacer");
+        int veces = sc.nextInt();
+        int x = 25;
+        int y = 90;
+
+        t.turnRight(y);
+        escalones(t,x,y,veces);
+        subir(t,x,y,veces);
+        t.forward(x);
+
+        t.show();
+    }
+    public static void escalones(Turtle t,int x, int y, int veces){
+        for (int i = 0; i < veces; i++) {
+            t.forward(x);
+            t.turnRight(y);
+            t.forward(x);
+            t.turnLeft(y);
+        }
+    }
+    public static void subir(Turtle t, int x,int y, int veces){
+        for (int i = 0; i < veces; i++) {
+            t.forward(x);
+            t.turnLeft(y);
+            t.forward(x);
+            t.turnRight(y);
+
+        }
+    }
+}
